f09e24024b1b0b0642c918b6fb76b8cf0136679f
Natalie <natalie.huang99@gmail.com>
2020-08-15 21:47:07 -0700

    Update SongRecTrigger.js

--- week2/homework/code/solution/SongRecTrigger.js
+++ week2/homework/code/solution/SongRecTrigger.js
@@ -1,15 +1,12 @@
 var multipart = require("parse-multipart");
-var request = require('request-promise');
+var fetch = require('node-fetch');
   
 module.exports = async function (context, req) {
     context.log('JavaScript HTTP trigger function processed a request.'); 
- 
-    console.log(req)
-    var boundary = multipart.getBoundary(req.headers['content-type']);
-    
-    var body = req.body;
 
-    var parts = multipart.Parse(body, boundary);
+    var boundary = multipart.getBoundary(req.headers['content-type']);
+    // parse the body
+    var parts = multipart.Parse(req.body, boundary);
     
     //analyze the image
     var result = await analyzeImage(parts[0].data);
@@ -19,44 +16,36 @@ module.exports = async function (context, req) {
             result
         }
     };
-    console.log(result);
+    console.log(result)
+    context.done(); 
 };
  
 async function analyzeImage(byteArray){
     
-    const subscriptionKey = 'YOUR SUBSCRIPTION KEY';
-    const uriBase = 'YOUR ENDPOINT' + '/face/v1.0/detect';
+    const subscriptionKey = '<YOUR SUBSCRIPTION KEY>';
+    const uriBase = '<YOUR ENDPOINT>' + '/face/v1.0/detect';
 
-    const params = {
+    let params = new URLSearchParams({
         'returnFaceId': 'true',
         'returnFaceAttributes': 'emotion'
-    };
+    })
 
-    const options = {
-        uri: uriBase,
-        qs: params,
+    let resp= await fetch(uriBase + `?${params.toString()}`, {
+        method: 'POST',
         body: byteArray,
         headers: {
             'Content-Type': "application/octet-stream",
             "Ocp-Apim-Subscription-Key": subscriptionKey
         }
-    }
-    let jsonResponse;
+    })
+
+    let data = await resp.json();
     
-    await request.post(options, (error, response, body) => {
-        if (error){
-            console.log('Error: ' + error);
-            return;
-        }
- 
-        jsonResponse = JSON.parse(body);
- 
-    });
-    return jsonResponse;  
+    return data; 
 }
 
 
-
+ 
 
 
 
